eb42a76fbb58d9caf704c299d84f7c4a2f9a13ec
Michael Wei <michael.wei@Michaels-Mac-mini.local>
2014-07-03 00:52:22 -0400

    asdf

--- scripts/test-suse.sh
+++
@@ -1,6 +0,0 @@
-Environment="Dev"
-
-Version="1.0"
-export Version
-
-. "$HOME/scripts/profile/1.0/setup_env.sh" $Environment
--- scripts/test-windows.sh
+++
@@ -1,9 +0,0 @@
-HOME="C:/Users/wmichae/Documents/My Projects/FlowPricerWebService/trunk/src/main/server_scripts"
-export HOME
-
-Environment="Test"
-
-Version="1.0"
-export Version
-
-. "$HOME/scripts/profile/1.0/setup_env.sh" $Environment
